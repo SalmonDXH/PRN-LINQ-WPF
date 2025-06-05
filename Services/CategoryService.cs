69459f5570efc15693431fe49bab81a20abaf731
Salmon <90376520+SalmonDXH@users.noreply.github.com>
2025-06-05 22:54:08 +0700

    Upload Services

---
+++ Services/CategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BusinessObjects;
+
+namespace Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryService iCategoryService;
+        public CategoryService() {
+            iCategoryService = new CategoryService();
+        }
+
+        public List<Category> GetCategories()
+        {
+            return iCategoryService.GetCategories();
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using BusinessObjects;
+using Repositories;
+using System.Collections.Generic;
+namespace Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductService iProductService;
+
+        public ProductService()
+        {
+            iProductService = new ProductService();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            iProductService.DeleteProduct(p);
+        }
+
+        public Product GetProductById(int id)
+        {
+            return iProductService.GetProductById(id);
+        }
+
+        public List<Product> GetProducts() {
+            return iProductService.GetProducts();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            iProductService.SaveProduct(p);
+        }
+
+        public void UpdateProduct(Product p)
+        {
+            iProductService.UpdateProduct(p);
+        }
+    }
+}
